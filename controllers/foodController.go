9da998b7fb314d10b8f429b9ca332347c4ba608f
hnsia <44498178+hnsia@users.noreply.github.com>
2024-05-03 00:00:33 +0800

    feat: add update menu method

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -80,16 +80,16 @@ func CreateFood() gin.HandlerFunc {
 	}
 }
 
-func round(num float64) int {
+// func round(num float64) int {
 
-}
+// }
 
-func toFixed(num float64, precision int) float64 {
+// func toFixed(num float64, precision int) float64 {
 	
-}
+// }
 
 func UpdateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		
 	}
 }
