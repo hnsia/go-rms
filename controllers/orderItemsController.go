8b06954a15d9cd303264c435f6a7477ec7eb676d
hnsia <44498178+hnsia@users.noreply.github.com>
2024-05-12 23:17:09 +0800

    feat: add get methods for order item

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,19 +1,56 @@
 package controller
 
 import (
+	"context"
+	"go-rms/database"
+	"go-rms/models"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type OrderItemPack struct {
+	Table_id *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing ordered items"})
+			return
+		}
+		var allOrderItems []bson.M
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			log.Fatal(err)
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
 func GetOrderItemsByOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		orderId := c.Param("order_id")
 
+		allOrderItems, err := ItemsByOrder(orderId)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing order items by order ID"})
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
@@ -23,7 +60,17 @@ func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
 
 func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		orderItemId := c.Param("order_item_id")
+		var orderItem models.OrderItem
+
+		err := orderItemCollection.FindOne(ctx, bson.M{"orderItem_id": orderItemId}).Decode(&orderItem)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing singular order item"})
+		}
+		c.JSON(http.StatusOK, orderItem)
 	}
 }
 
