80eaf60d9891988193e518d8ec9321e7b8ecc98f
hnsia <44498178+hnsia@users.noreply.github.com>
2024-05-18 17:44:56 +0800

    feat: add get methods to table

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,9 +1,15 @@
 package controller
 
 import (
+	"context"
 	"go-rms/database"
+	"go-rms/models"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,13 +17,33 @@ var tableCollection *mongo.Collection = database.OpenCollection(database.Client,
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing tables"})
+		}
+		var alltables []bson.M
+		if err = result.All(ctx, &alltables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, alltables)
 	}
 }
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+		var table models.Table
 
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured wile fetching the tables"})
+		}
+		c.JSON(http.StatusOK, table)
 	}
 }
 
